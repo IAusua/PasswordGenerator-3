4bc894200358ca27831e378688c52a1dc2a92496
Ismael <ausuaismael@gmail.com>
2022-12-19 23:12:09 -0500

    t commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PasswordGenerator-3
+Tried making a password generator work. It would have asked the user how long their password would be, and if they would like uppercase, lowercase, and special characters.
+It would then generate a password for the user that they would be able to use. 
+
+file:///C:/Users/ausua/BootcampWork/PasswordGenerator-3/index.html
+
+https://github.com/IAusua/PasswordGenerator-3
+
--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword(){
+  let PNumber = "1234567890"
+  let Puppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let Plowercase = "abcdefghijklmnopqrstuvwxyz";
+  let Pspecial = "@#$%^&*()_+><?{}[]'"; 
+  
+
+    var number = prompt("How long would you like your password?");
+  if (prompt === null){
+      return;
+    }
+
+    if(prompt )
+
+    var uppercase = prompt("Would you like uppercase characters?");
+  if (prompt === null){
+    return;
+  }
+  
+ 
+  var lowercase = prompt("Would you like lowercase characters?");
+  if (prompt === null){
+    return;
+  }
+  
+  var special = prompt("Would you like special characters?");
+  if (prompt === null){
+    return;
+
+    
+  }
+
+  if (!generateBtn){
+    return;
+  }
+ 
+
+
+
+}
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -13,5 +52,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
